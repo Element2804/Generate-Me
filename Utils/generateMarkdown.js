d07f5606e0d5c6b710f30769765ed1f91428c615
Element2804 <103654389+Element2804@users.noreply.github.com>
2023-04-21 11:37:58 -0400

    got license to work

--- Utils/generateMarkdown.js
+++ Utils/generateMarkdown.js
@@ -1,19 +1,77 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(data) {
+    const licenseType = data.license;
+    
+    
+    let licenseString = " "
+
+    if (licenseType === "MIT") {
+      licenseString = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`
+    };
+   if (licenseType === "BSD") {
+      licenseString = `[![License: BSD](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]`
+   };
+    if (licenseType === "Apache") {
+       licenseString = `[![License: Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]`
+      };
+    if (licenseType === "AGPL") {
+        licenseString = `[![License:GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]`
+      };
+      console.log(data.license);
+    return licenseString
+    
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+//function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+/* function renderLicenseSection(license) {
+    const licenses = {
+        MIT: {
+            badge:
+        }
+    }
+} */
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+ 
+## Description
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+In order to run, Node.js and the Inquirer NPM need to be installed. Then open file in Visual Studio code and pull up the terminal.
+
+
+## Usage
+
+After all dependencies have been installed. Type "node index.js" in to the terminal. Then follow prompts to generate a custom README
+
+## License
+${renderLicenseBadge(data)}
+
+## Github
+${data.github}
+
+## Email
+${data.email}
+
+
 `;
 }
+
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Includes packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./Utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [{
@@ -14,7 +15,7 @@ const questions = [{
     message: 'Please enter a short description of your project.',    
      },
   {
-    type: 'checkbox',
+    type: 'list',
     message: 'Please select your preferred license.',
     name: 'license',
     choices: ['MIT', 'BSD', 'Apache', 'AGPL'],
@@ -32,28 +33,29 @@ const questions = [{
 
 ];
 
-//console.log(questions)
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     
+    const content = generateMarkdown(data);
    fs.writeFile(fileName, content, function (error) {
         if (error) {
             return console.log(error)
         }
         console.log('success')
     });
-}
+} 
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer
     .prompt(questions)
-
-    .then((data) => {
-        
-        console.log(data)
-        writeToFile();
+   .then((data) => {
+    
+        const fileName = "README.md";
+     
+        writeToFile(fileName, data);
       });
 }
 
