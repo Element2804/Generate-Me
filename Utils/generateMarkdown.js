124fffb395520ea0fc613b41645312138794ec4a
Element2804 <103654389+Element2804@users.noreply.github.com>
2023-04-24 11:24:05 -0400

    reordered table of contents

--- Utils/generateMarkdown.js
+++ Utils/generateMarkdown.js
@@ -1,34 +1,32 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// attaches badges to the license depending on the user selection
 function renderLicenseBadge(data) {
-    const licenseType = data.license;
-    
-    
+    const licenseType = data.license;    
     let licenseString = " "
 
     if (licenseType === "MIT") {
-      licenseString = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`
+      licenseString = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
     };
    if (licenseType === "BSD") {
-      licenseString = `[![License: BSD](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]`
+      licenseString = `![License: BSD](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)`
    };
     if (licenseType === "Apache") {
-       licenseString = `[![License: Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]`
+       licenseString = `![License: Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`
       };
     if (licenseType === "AGPL") {
-        licenseString = `[![License:GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]`
+        licenseString = `![License:GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
       };
    
     return licenseString
     
 }
 
-
+// creates the structure of the README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# <h1>${data.title}</h1>
 
 ## License
   This project is covered under the ${data.license} license.
+  <br>
   ${renderLicenseBadge(data)} 
  
 ## Description
@@ -36,10 +34,10 @@ ${data.description}
 
 ## Table of Contents
 
+- [Description](#description)
 - [Installation](#installation)
 - [Usage](#usage)
 - [Test](#test)
-- [Credits](#credits)
 - [Github](#github)
 - [Contribution](#contribution)
 - [Questions](#questions)
@@ -66,5 +64,5 @@ If you have any questions feel free to email me at ${data.email}
 `;
 }
 
-
+// exports the file so its accessible by index.js
 module.exports = generateMarkdown;
